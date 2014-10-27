2b7584e369c475049e2a89e509f0c1c3669071ff
maheshakya <pmaheshakya4@gmail.com>
2014-10-27 13:04:51 +0530

    Added Gausssain Kernel

---
+++ src/com/gsom/util/GSOMConstants.java
@@ -0,0 +1,22 @@
+package com.gsom.util;
+
+
+public class GSOMConstants {
+
+	public static int DIMENSIONS;
+
+	private static double GT;
+
+        public static double SPREAD_FACTOR;
+	public static double MAX_NEIGHBORHOOD_RADIUS;
+	public static double FD;
+	public static double START_LEARNING_RATE;
+	public static int MAX_ITERATIONS;
+        
+        public static double GAMMA_FOR_GAUSSIAN = 0.4;
+        
+        public static double getGT(){
+            GT = - DIMENSIONS * DIMENSIONS * Math.log(SPREAD_FACTOR);
+            return GT;
+        }
+}
